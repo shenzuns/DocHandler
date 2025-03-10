fe79a353b790bcdd012390fcfc6abc0963e039cf
sxs <3072144658@qq.com>
2025-03-10 16:15:43 +0800

    创建工程

---
+++ src/main/java/com/example/dochandler/DocHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dochandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocHandlerApplication.class, args);
+	}
+
+}
